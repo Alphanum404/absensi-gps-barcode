7d0fc635ac9fea38e920f52f0ba9c6facf4131f4
Alphanum404 <aldiem2004@gmail.com>
2025-05-04 10:29:46 +0700

    feat: Implement event retrieval and upcoming events check in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Event;
 
 class HomeController extends Controller
 {
@@ -11,6 +13,23 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('home');
+        // Get active event for today
+        $event = Event::where(function ($query) {
+            $query->whereDate('event_date', now()->toDateString());
+        })->orderBy('start_time')->first();
+
+        return view('home', compact('event'));
+    }
+
+    // Tambahkan method ini untuk memeriksa apakah ada event yang akan datang
+    public function hasUpcomingEvents()
+    {
+        $today = Carbon::today();
+        $tomorrow = Carbon::tomorrow();
+
+        return Event::where(function ($query) use ($today, $tomorrow) {
+            $query->whereDate('event_date', $today)
+                ->orWhereDate('event_date', $tomorrow);
+        })->exists();
     }
 }
